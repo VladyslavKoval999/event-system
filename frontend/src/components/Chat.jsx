47e702c03bca7e0b37b18a03349ae086bde6ecca
VladyslavKoval999 <koval671219@gmail.com>
2026-03-28 16:13:01 +0200

    Підключив фронтенд до хмарного бекенду

--- frontend/src/components/Chat.jsx
+++ frontend/src/components/Chat.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { io } from 'socket.io-client';
 
-const socket = io('http://localhost:3000'); 
+const socket = io('https://event-backend-api-u0ur.onrender.com');
 
 export default function Chat() {
     const [messages, setMessages] = useState([]);
